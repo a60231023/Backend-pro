eb233e034c03a6f4d4ed4ed8040a7ee2fa5e63ff
a60231023 <ayushtiwari6061@gmail.com>
2023-04-12 19:55:20 +0530

    adding method to the user object

--- eCommerse/models/user.js
+++ eCommerse/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -41,7 +43,7 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-//encrypt password before save
+//encrypt password before save -- HOOKS
 //using pre hooks -- cannot use arrow function inside it
 // we are using next as after this process is done pass it on the next item
 //but it will run all the time where there is updating but we want to run it for first time so we use 
@@ -49,8 +51,36 @@ userSchema.pre('save', async function(next){
     //only run this function if password was modified (not on toher modification or upate function)
     if(!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
-})
+});
 
+//adding method to userSchema object . this method will be available to all the object of userSchema
+// validate the password with passed on user password
+userSchema.methods.isVaildatePassword = async function(sendPassword){
+    // boolean value compare function returns
+    return await bcrypt.compare(sendPassword, this.password);
+};
 
+//create and return jwt token
+userSchema.methods.getJwtToken = function(){
+    jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRY
+    });
+};
+
+//generate forgot password token(string)
+userSchema.methods.getForgotPasswordToken = function(){
+    //generate a long random string- multiple methods 
+    const forgotToken = crypto.randomBytes(20).toString('hex');
+    //the above thing generates it and you can use it directly but we are adding one extra layer of security-- it can be ignored
+
+    // getting a hash - make sure to get a hash on backend
+    //we are saving the hash in the db but sending the forgottoken without the hash so when the user sends -- hash it then compare
+    this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest('hex');
+
+    //time of token
+    this.forgotPasswordExpiry = Date.now() + 20*60*1000;
+
+    return forgotToken;
+};
 
 module.exports = mongoose.model('User', userSchema);
