b380c7b7bd28cedafd561bd3e59ba1b393a07d71
a60231023 <ayushtiwari6061@gmail.com>
2023-04-27 09:18:27 +0530

    admin get and update route done

--- eCommerse/controllers/productController.js
+++ eCommerse/controllers/productController.js
@@ -4,75 +4,141 @@ const CustomError = require("../utils/customError");
 const cloudinary = require("cloudinary");
 const WhereClause = require("../utils/whereClause");
 
-
-
-
 exports.addProduct = BigPromise(async (req, res, next) => {
-    let imageArray = [];
+  let imageArray = [];
 
-    if(!req.files){
-        return next(new CustomError('Images are required', 401));
-    }
+  if (!req.files) {
+    return next(new CustomError("Images are required", 401));
+  }
 
-    if(req.files){
-        for(let index = 0; index<req.files.photos.length; index++){
-            let result = await cloudinary.v2.uploader.upload(req.files.photos[index].tempFilePath, {
-                folder: "Products"
-            });
-
-            imageArray.push({
-                id: result.public_id,
-                secure_url: result.secure_url,
-            });
+  if (req.files) {
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "Products",
         }
+      );
+
+      imageArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
     }
+  }
 
-    req.body.photos = imageArray;
-    //the user is already logged so it will req.user property
-    req.body.user = req.user.id;
+  req.body.photos = imageArray;
+  //the user is already logged so it will req.user property
+  req.body.user = req.user.id;
 
-    const product = await Product.create(req.body);
+  const product = await Product.create(req.body);
 
-    res.status(200).json({
-        success: true,
-        product,
-    });
-
-
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
 
 exports.getAllProduct = BigPromise(async (req, res) => {
-    
-    const resultPerPage = 6;    
-    const countProduct = await Product.countDocuments();
-    
-    // const products = await Product.find({});
+  const resultPerPage = 6;
+  const countProduct = await Product.countDocuments();
 
-    const productsObj = new WhereClause(Product.find(), req.query).search().filter();
-    let products = productsObj.base;
-    const filteredProductLength = products.length;
+  // const products = await Product.find({});
 
-    productsObj.pager(resultPerPage);
-    products = await productsObj.base;
+  const productsObj = new WhereClause(Product.find(), req.query)
+    .search()
+    .filter();
+  let products = productsObj.base;
+  const filteredProductLength = products.length;
 
+  productsObj.pager(resultPerPage);
+  products = await productsObj.base;
 
-
-    res.status(200).json({
-        success: true,
-        products,
-        countProduct,
-        filteredProductLength,
-    })
-
-
+  res.status(200).json({
+    success: true,
+    products,
+    countProduct,
+    filteredProductLength,
+  });
 });
 
+exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+exports.adminUpdateOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+  let imagesArray = [];
+
+  if (req.files) {
+    //destroy the existing image
+    for (let index = 0; index < product.photos.length; index++) {
+      const res = await cloudinary.v2.uploader.destroy(
+        product.photos[index].id
+      );
+    }
+
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "products", //folder name -> .env
+        }
+      );
+
+      imagesArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  req.body.photos = imagesArray;
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+
+  //destroy the existing image
+  for (let index = 0; index < product.photos.length; index++) {
+    const res = await cloudinary.v2.uploader.destroy(product.photos[index].id);
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product was deleted !",
+  });
+});
 
 exports.testProduct = (req, res) => {
-    res.status(200).json({
-        success: true,
-        greetings: "Hello from API"
-    });
+  res.status(200).json({
+    success: true,
+    greetings: "Hello from API",
+  });
 };
-
-
--- eCommerse/routes/productRoute.js
+++ eCommerse/routes/productRoute.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const { testProduct , addProduct, getAllProduct} = require("../controllers/productController");
+const {
+  testProduct,
+  addProduct,
+  getAllProduct,
+  adminGetAllProduct,
+  adminUpdateOneProduct
+} = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/user");
-
 
 router.get("/testProduct", testProduct);
 router.get("/products", getAllProduct);
 
 //adming add product route
-router.post("/admin/product/add", isLoggedIn, customRole('admin'), addProduct);
+router.post("/admin/product/add", isLoggedIn, customRole("admin"), addProduct);
+router.get(
+  "/admin/product/get",
+  isLoggedIn,
+  customRole("admin"),
+  adminGetAllProduct
+);
+router.put(
+    "/admin/product/update",
+    isLoggedIn,
+    customRole("admin"),
+    adminUpdateOneProduct
+  );
+
 
 
 module.exports = router;
