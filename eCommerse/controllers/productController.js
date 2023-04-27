d846fcd6e829ff67a27ca8737ce9ed263e2a7ad2
a60231023 <ayushtiwari6061@gmail.com>
2023-04-27 10:37:08 +0530

    add, delete and get review done

--- eCommerse/controllers/productController.js
+++ eCommerse/controllers/productController.js
@@ -62,19 +62,107 @@ exports.getAllProduct = BigPromise(async (req, res) => {
   });
 });
 
-
 exports.getOneProduct = BigPromise(async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-  
-    if (!product) {
-      return next(new CustomError("No product found with this id", 401));
-    }
-    res.status(200).json({
-      success: true,
-      product,
-    });
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+  res.status(200).json({
+    success: true,
+    product,
   });
-  
+});
+
+exports.addReview = BigPromise(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const AlreadyReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (AlreadyReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  // adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //save
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.deleteReview = BigPromise(async (req, res, next) => {
+  const { productId } = req.query;
+
+  const product = await Product.findById(productId);
+
+  const reviews = product.reviews.filter(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  const numberOfReviews = reviews.length;
+
+  // adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //update the product
+
+  await Product.findByIdAndUpdate(
+    productId,
+    {
+      reviews,
+      ratings,
+      numberOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.getOnlyReviewsForOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
 
 exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
   const products = await Product.find();
--- eCommerse/routes/productRoute.js
+++ eCommerse/routes/productRoute.js
@@ -7,13 +7,19 @@ const {
   adminGetAllProduct,
   adminUpdateOneProduct,
   adminDeleteOneProduct,
-  getOneProduct
+  getOneProduct,
+  addReview,
+  deleteReview,
+  getOnlyReviewsForOneProduct
 } = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 router.get("/testProduct", testProduct);
 router.get("/products", getAllProduct);
 router.get("/product:id", getOneProduct);
+router.put("/review", isLoggedIn, addReview);
+router.delete("/review", isLoggedIn, deleteReview);
+router.get("/reviews", isLoggedIn, getOnlyReviewsForOneProduct);
 
 
 //adming add product route
